d05c66f674f5debb33dd1ed74f3cecc4dece5a16
Notsponge <arevadzezu2009@gmail.com>
2024-12-14 18:32:30 +0200

    Add files via upload

---
+++ Sashinao JS-5/index.js
@@ -0,0 +1,83 @@
+//Task 1
+
+
+// for (let i = 150; i > 0; i--){
+//   console.log(i)
+// }
+
+
+
+// for (let i = 150; i > 0; i -= 2){
+//   console.log(i)
+// }
+
+
+
+// for (let i = 150; i > 0; i -= 3){
+//   console.log(i)
+// }
+
+
+
+// for (let i = 0; i <= 150; i += 2){
+//   console.log(i)
+// }
+
+
+
+// for (let i = 0; i <= 150; i += 3){
+//   console.log(i)
+// }
+
+
+
+// for (let i = 0; i <= 150; i += 4){
+//   console.log(i)
+// }
+
+
+
+
+// let i = 150;
+
+// while(i>0){
+//   console.log(i--);
+// }
+
+
+
+// let i = 150;
+
+// while(i>0){
+//   console.log(i -= 2)
+// }
+
+
+
+// let i = 150;
+
+// while(i>0){
+//   console.log(i -= 3)
+// }
+
+
+
+// let i = 0;
+
+// while(i<150){
+//   console.log(i++);
+// }
+
+
+// let i = 0;
+
+// while(i<150){
+//   console.log( i += 2)
+// }
+
+
+// let i = 0;
+
+// while(i <= 150){
+//   console.log(i += 4);
+// }
